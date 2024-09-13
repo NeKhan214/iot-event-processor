c5a741ad396f17fd60a6cd01be05aa8f9e4cff4d
NehaK214 <er.nehakhan@gmail.com>
2024-09-13 20:02:06 +0530

    Adding IOT Application class IOT Event API

---
+++ src/main/java/com/nekhan/iot/event/processor/IotEventProcessorApplication.java
@@ -0,0 +1,30 @@
+package com.nekhan.iot.event.processor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * The main entry point for the IoT Event Processor Spring Boot Application.
+ * <p>
+ * This class is responsible for bootstrapping the Spring Boot application.
+ * </p>
+ *
+ * @author Neha Khan
+ * @version 1.0
+ */
+@SpringBootApplication
+public class IotEventProcessorApplication {
+
+    /**
+     * The main method that serves as the entry point of the application.
+     * <p>
+     * This method initializes the Spring Boot application context.
+     * </p>
+     *
+     * @param args command-line arguments
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(IotEventProcessorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nekhan/iot/event/processor/IotEventProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nekhan.iot.event.processor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IotEventProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
